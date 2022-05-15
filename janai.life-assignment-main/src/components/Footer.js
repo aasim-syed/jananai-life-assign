48fa44087dca65a9475be7ee1dd66d73eb2c0ee5
syed aasim <syed-aasim>
2022-05-15 14:03:53 +0530

    new file:   janai.life-assignment-main/.gitignore 	new file:   janai.life-assignment-main/README.md 	new file:   janai.life-assignment-main/package-lock.json 	new file:   janai.life-assignment-main/package.json 	new file:   janai.life-assignment-main/public/Logo2.png 	new file:   janai.life-assignment-main/public/favicon.ico 	new file:   janai.life-assignment-main/public/index.html 	new file:   janai.life-assignment-main/public/manifest.json 	new file:   janai.life-assignment-main/public/robots.txt 	new file:   janai.life-assignment-main/src/App.js 	new file:   janai.life-assignment-main/src/State.js 	new file:   janai.life-assignment-main/src/components/ActorInfoPage.js 	new file:   janai.life-assignment-main/src/components/Footer.js 	new file:   janai.life-assignment-main/src/components/Header.js 	new file:   janai.life-assignment-main/src/components/Logo2.png 	new file:   janai.life-assignment-main/src/components/Main.js 	new file:   janai.life-assignment-main/src/components/MovieItem.js 	new file:   janai.life-assignment-main/src/components/MoviePage.js 	new file:   janai.life-assignment-main/src/components/MoviesGrid.js 	new file:   janai.life-assignment-main/src/components/SearchBox.js 	new file:   janai.life-assignment-main/src/index.js 	new file:   janai.life-assignment-main/src/styles.css

---
+++ janai.life-assignment-main/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            
+          </div>
+        </div>
+        <hr />
+        <div className="row">
+          <p className="col-sm" id="right">
+            &copy;{new Date().getFullYear()} DONT KNOW WHAT TO ADD HERE SO JUST USED Date() function😢😒
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
